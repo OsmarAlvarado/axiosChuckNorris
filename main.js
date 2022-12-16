70134663b2fb76c797506ed8563fb6c7c6984ea9
OsmarAlvarado <osmar.alvarado.dev@gmail.com>
2022-12-16 17:29:13 +0100

    final

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+axios
+    .get(`https://api.chucknorris.io/jokes/categories`)
+    .then(response => {
+        const jokesCategories = response.data
+        const allCategories = []
+
+        console.log(response);
+
+        jokesCategories.forEach(categorie => {
+
+            let categoriesList = document.createElement('div')
+            document.querySelector('body').appendChild(categoriesList)
+            categoriesList.innerHTML = `                
+                <a href="#" class='categories'>${categorie}</a>                
+                ` //me muestra el nombre de las categorias en los links
+            allCategories.push(categorie)
+        })
+
+        const toClick = document.querySelectorAll('.categories')
+
+        let categoriesList = document.createElement('div')
+
+        toClick.forEach(click => {
+            click.onclick = () => {
+                axios
+                    .get(`https://api.chucknorris.io/jokes/random?category=${click.innerText}`)
+                    .then(response => {
+                        //let categoriesList = document.createElement('div') //aqui no se me borran por cada click
+
+                        document.querySelector('body').appendChild(categoriesList)
+                        const Joke = response.data.value
+                        categoriesList.innerHTML = `JOKE: <p id="jokeP">${Joke}<p>`
+                    })
+            }
+        })
+
+
+    })
